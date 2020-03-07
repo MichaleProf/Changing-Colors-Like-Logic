7808fda536ccd623fd1348f31e98530cc39f0f9c
MichaleProf <59575961+MichaleProf@users.noreply.github.com>
2020-03-07 11:53:24 -0500

    paintbuckets and improved player diagonal movement

--- src/Board.java
+++ src/Board.java
@@ -16,6 +16,7 @@ public class Board extends JPanel implements ActionListener{
 
 
 
+
     public Board(Game game){
 
         this.game = game;
@@ -28,12 +29,73 @@ public class Board extends JPanel implements ActionListener{
 
     public void setup(){
 
-        Player player = new Player(Color.GREEN, WIDTH/2, HEIGHT/2, 10, 5, this, 10, game);
-        actors.add(player);
+        actors = new ArrayList<>();
+
+        actors.add(new Player(Color.GREEN, WIDTH/2, HEIGHT/2, 20, 10, this, 10, game));
+
+        //init buckets
+        for(int i = 0; i < STATS.getNumBuckets(); i++){
+            Color color = Color.GREEN;
+            int x = 0;
+            int y = 0;
+            int diameter = 20;
+            int height = HEIGHT - diameter;
+            int width = WIDTH - diameter;
+
+            switch(i){
+                case 0:
+                    color = Color.GREEN;
+                    x = 0;
+                    y = 0;
+                    break;
+                case 1:
+                    color = Color.BLUE;
+                    x = width;
+                    y = 0;
+                    break;
+                case 2:
+                    color = Color.RED;
+                    x = width;
+                    y = height;
+                    break;
+                case 3:
+                    color = Color.ORANGE;
+                    x = 0;
+                    y = height;
+                    break;
+                case 4:
+                    color = Color.PINK;
+                    x = width/2;
+                    y = 0;
+                    break;
+                case 5:
+                    color = Color.YELLOW;
+                    x = width;
+                    y = height/2;
+                    break;
+                case 6:
+                    color = Color.MAGENTA;
+                    x = width/2;
+                    y = height;
+                    break;
+                case 7:
+                    color = Color.ORANGE;
+                    x = 0;
+                    y = height/2;
+                    break;
+            }
+
+            actors.add(new PaintBucket(color, x, y, diameter, diameter, this));
+        }
 
     }
 
     public void checkCollisions(){
+
+        for(int i = 1; i < STATS.getNumBuckets() + 1; i++){
+            if(actors.get(0).collidesWith(actors.get(i)))
+                actors.get(0).setColor(actors.get(i).getColor());
+        }
 
     }
 
@@ -43,6 +105,8 @@ public class Board extends JPanel implements ActionListener{
         for(Sprite actor: actors){
             actor.move();
         }
+
+        checkCollisions();
 
         repaint();
 
